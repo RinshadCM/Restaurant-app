c5f867a95bd1dc922486f4dd3fe93bb2a78c8580
Mohammed Rinshad C M <rinshadcm34@gmail.com>
2023-03-11 10:56:04 +0530

    Updates

---
+++ precache-manifest.39f6b6d618620521b923fb93e20d5013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc2ca05e7e8c100b5ac3",
+    "url": "/RestaurantGuide/static/css/main.00ab4752.chunk.css"
+  },
+  {
+    "revision": "fc2ca05e7e8c100b5ac3",
+    "url": "/RestaurantGuide/static/js/main.fc2ca05e.chunk.js"
+  },
+  {
+    "revision": "e924beb03933b8c1ef95",
+    "url": "/RestaurantGuide/static/js/1.e924beb0.chunk.js"
+  },
+  {
+    "revision": "7582c4b538b7ef629ed9",
+    "url": "/RestaurantGuide/static/js/2.7582c4b5.chunk.js"
+  },
+  {
+    "revision": "ad625db1585de8ffed58",
+    "url": "/RestaurantGuide/static/js/runtime~main.ad625db1.js"
+  },
+  {
+    "revision": "55373925645d42db557969db62c2e332",
+    "url": "/RestaurantGuide/index.html"
+  }
+];
